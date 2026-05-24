3298f2a2ab134a7e3a7e968fcb7bbb6613f4c80e
GameMaster <281981779+GameMaster97@users.noreply.github.com>
2026-05-24 08:43:32 -0700

    fancify

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,34 @@
 const titleElement = document.getElementById("main-title");
-const originalTitleContent = titleElement.textContent.split("");
-const originalLen = originalTitleContent.length;
-let len = 0;
-let currentText = "";
-titleElement.textContent = "";
-const intervalId = setInterval(() => {
-    currentText = currentText + originalTitleContent[len];
-    titleElement.textContent = currentText + "|";
-
-    console.log(`Status: ${len} // ${originalLen}`)
+const sloganElement = document.getElementById("slogan");
+var typeEffect = (element, spacingTimeMS, endingFunction) => {
+    // Reset the original text
+    element.textContent = "";
     
-    len++;
-    if (len === originalLen) {
-        titleElement.textContent = currentText; 
-        clearInterval(intervalId);
-    }
-}, 100);
+    // Variables to help us
+    const originalElementContent = element.textContent.split("");
+    const originalLen = originalElementContent.length;
+    let len = 0;
+    let currentText = "";
+
+    const intervalId = setInterval(() => {
+        currentText = currentText + originalElementContent[len];
+        element.textContent = currentText + "|";
+
+        console.log(`Status: ${len} // ${originalLen}`);
+    
+        len++;
+        if (len === originalLen) {
+            element.textContent = currentText; 
+            clearInterval(intervalId);
+            if (endingFunction !== undefined) {
+                endingFunction()
+            }
+        }
+    }, spacingTimeMS);
+}
+
+typeEffect(titleElement, 100, () => {
+    setTimeout(() => {
+        typeEffect(sloganElement, 125);
+    }, 750);
+});
