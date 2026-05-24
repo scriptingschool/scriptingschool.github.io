aab8952cf9008347bcf8b11aad6aa79898fb7559
GameMaster <281981779+GameMaster97@users.noreply.github.com>
2026-05-23 18:25:04 -0700

    do stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+const titleElement = document.getElementById("main-title");
+const originalTitleContent = titleElement.textContent.split("");
+const originalLen = 0;
+let len = 0;
+let currentText = "";
+titleElement.textContent = "";
+const intervalId = setInterval(() => {
+    currentText = currentText + originalTitleContent[len];
+    titleElement.textContent = currentText + "|";
+    
+    len++;
+    if (len === originalLen) {
+        clearInterval(intervalId);
+    }
+}, 100);
